d7c66fce64bbb8c89b8a8fe38cc0daf2d3eb9bf5
Nikita Rodionov <proud9proud@gmail.com>
2025-10-29 12:20:23 +0300

    Fixes

--- Demo/TONWalletApp/Password/CreatePassword/CreatePasswordView.swift
+++ Demo/TONWalletApp/Password/CreatePassword/CreatePasswordView.swift
@@ -12,10 +12,10 @@
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -93,8 +93,7 @@ struct CreatePasswordView: View {
                 Text("⚠️")
                     .textSM()
                 
-                Text("Make sure to remember your password.
-It cannot be recovered if forgotten.")
+                Text("Make sure to remember your password.\nIt cannot be recovered if forgotten.")
                     .textSM()
                     .foregroundColor(Color.TON.gray500)
             }
--- Sources/TONWalletKit/JSWalletKit/JSWalletKitFile.swift
+++ Sources/TONWalletKit/JSWalletKit/JSWalletKitFile.swift
@@ -12,10 +12,10 @@
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,9 +42,7 @@ struct JSWalletKitScript: JSScript {
         var code = try String(contentsOfFile: path, encoding: .utf8)
         
         code = code.replacingOccurrences(
-            of: "export {
-  A3 as main
-};",
+            of: "export {\n  A3 as main\n};",
             with: """
             // Make main function available globally for JavaScriptCore
             var main = A3;
