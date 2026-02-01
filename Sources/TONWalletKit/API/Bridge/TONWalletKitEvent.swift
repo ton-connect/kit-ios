8486ef0373fe76d34f8c8e63e605bd4f9602c077
Nikita Rodionov <proud9proud@gmail.com>
2026-02-02 02:30:54 +0300

    -

--- Sources/TONWalletKit/API/Bridge/TONWalletKitEvent.swift
+++ Sources/TONWalletKit/API/Bridge/TONWalletKitEvent.swift
@@ -12,10 +12,10 @@
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//  
+//
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-//  
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -46,6 +46,22 @@ public enum TONWalletKitEvent {
         case .disconnect:
             let event: TONDisconnectionEvent = try bridgeEvent.value.decode()
             self = .disconnect(event)
+        }
+    }
+}
+
+public extension TONWalletKitEvent {
+    
+    var isJsBridge: Bool {
+        switch self {
+        case .connectRequest(let request):
+            return request.event.isJsBridge == true
+        case .transactionRequest(let request):
+            return request.event.isJsBridge == true
+        case .signDataRequest(let request):
+            return request.event.isJsBridge == true
+        case .disconnect(let event):
+            return event.isJsBridge == true
         }
     }
 }
